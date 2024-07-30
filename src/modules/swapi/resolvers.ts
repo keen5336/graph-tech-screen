a921ba27454f01075cc64966482885658acb1ac3
Adam Kilber <keen5336@gmail.com>
2024-07-30 17:25:30 -0500

    updated resolver to query swapi.dev api to get star wars characters.

--- src/modules/swapi/resolvers.ts
+++ src/modules/swapi/resolvers.ts
@@ -1,31 +1,49 @@
-import { Context } from "apollo-server-core";
-import { GraphQLFieldResolver } from "graphql";
+import { Context } from 'apollo-server-core'
+import { GraphQLFieldResolver } from 'graphql'
 
-type Args = { id: string };
+type Args = { id: string }
+
+interface Character {
+	name: string
+	height: string
+	mass: string
+	gender: string
+}
+
+const genderMap: { [key: string]: string } = {
+	male: 'MALE',
+	female: 'FEMALE',
+}
 
 const Query: Record<string, GraphQLFieldResolver<{}, Context, any>> = {
-  swapiCharacterById: async (_, args: Args, ctx) => {
-    return {};
-  },
-};
+	swapiCharacterById: async (_, args: Args, ctx) => {
+		const response = await fetch(`https://swapi.dev/api/people/${args.id}/`)
+		const data: Character = await response.json()
+		return {
+			name: data.name,
+			height: data.height,
+			mass: data.mass,
+			gender: data.gender,
+		}
+	},
+}
 
 const StarWarsCharacter = {
-  name: () => {
-    return "test";
-  },
-  height: () => {
-    return "171";
-  },
-  mass: () => {
-    return "100";
-  },
-  gender: () => {
-    return "MALE";
-  },
-};
+	name: (parent: Character) => {
+		return parent.name
+	},
+	height: (parent: Character) => {
+		return parent.height
+	},
+	mass: (parent: Character) => {
+		return parent.mass
+	},
+	gender: (parent: Character) => {
+		return genderMap[parent.gender.toLowerCase()] || null
+	},
+}
 
-// You can add new Object Resolvers to the default export and the server will pick them up automatically
 export default {
-  Query,
-  StarWarsCharacter,
-};
+	Query,
+	StarWarsCharacter,
+}
